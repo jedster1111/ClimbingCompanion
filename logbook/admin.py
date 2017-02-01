6abfd0008239f8228599792fe842e3cde621b383
Jed <jedster1111@hotmail.co.uk>
2017-02-01 23:10:52 +0000

    created test models for logbook and registered in admin

--- logbook/admin.py
+++ logbook/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
-from . models import Coder
+from . models import Coder, Centre, Climb
 
 admin.site.register(Coder)
+admin.site.register(Centre)
+admin.site.register(Climb)
+
--- logbook/models.py
+++ logbook/models.py
@@ -12,4 +12,22 @@ class Coder(models.Model):
     def __str__(self):
         return self.first_name + " " + self.last_name
 
+class Centre(models.Model):
+    name = models.CharField(max_length=100)
+    nearest_city = models.CharField(max_length=50)
+        
+    def __str__(self):
+        fullName = self.name + " (" + self.nearest_city + ")"
+        return fullName
+
+class Climb(models.Model):
+    colour = models.CharField(max_length=50)
+    grade = models.CharField(max_length=6)
+    centre = models.ForeignKey(Centre, on_delete=models.CASCADE)
+
+    def __str__(self):
+        routeName = self.colour + " " + self.grade
+        return routeName
+
+
 
