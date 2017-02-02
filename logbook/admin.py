36749a3f8116249ea31ed5dff9d704ebdc1ce77a
Jed <jedster1111@hotmail.co.uk>
2017-02-02 15:21:13 +0000

    added climbs inline to centre admin

--- logbook/admin.py
+++ logbook/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from . models import Coder, Centre, Climb
 
+class ClimbInline(admin.StackedInline):
+    model = Climb
+
+class CentreAdmin(admin.ModelAdmin):
+    inlines = [ClimbInline,]
+
 admin.site.register(Coder)
-admin.site.register(Centre)
+admin.site.register(Centre, CentreAdmin)
 admin.site.register(Climb)
 
