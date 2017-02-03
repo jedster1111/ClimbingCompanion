09246026ab2b75515a2dcf3edbbf2c39c0078714
Jed <jedster1111@hotmail.co.uk>
2017-02-03 07:43:43 +0000

    went back to where I was before

--- logbook/static/logbook/js/controllers.js
+++
@@ -1,5 +0,0 @@
-﻿angular.module('logbookApp', []).controller('ClimbListController', function ($scope, $state, $window, Climb) {
-    $scope.climbs = Climb.query();
-}).controller('ClimbViewController', function ($scope, $stateParams, Climb) {
-    $scope.Climb = Climb.get({ id: $stateParams.id });
-});
--- logbook/static/logbook/js/services.js
+++
@@ -1,7 +0,0 @@
-﻿angular.module('logbookApp.services', []).factory('Climb', function ($resource) {
-    return $resource('http://127.0.0.1:8000/logbook/climbs/:id', {
-        update: {
-            method: 'PUT'
-        }
-    });
-});
