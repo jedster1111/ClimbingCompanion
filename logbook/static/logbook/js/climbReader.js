21f2cabdc5a33034b7b95e2a675b4c0fd502b457
Jed <jedster1111@hotmail.co.uk>
2017-02-03 08:30:16 +0000

    added a http request to climbs api and then prints a list of all climbs

---
+++ logbook/static/logbook/js/climbReader.js
@@ -0,0 +1,14 @@
+﻿var app = angular.module('logbookApp', []);
+
+app.config(function ($interpolateProvider) {
+    $interpolateProvider.startSymbol('[[');
+    $interpolateProvider.endSymbol(']]');
+});
+
+
+app.controller('climbReader', function ($scope, $http) {
+    $http.get('http://127.0.0.1:8000/logbook/climbs/')
+        .then(function (response) {
+            $scope.climbs = response;
+        });
+});
