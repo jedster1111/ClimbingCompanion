f33c816b7b0cab3cd2cc60f1f20cad17ea6237f4
Jed <jedster1111@hotmail.co.uk>
2017-02-03 04:22:21 +0000

    added a schema, tbh not sure what it's for but whatever

--- logbook/urls.py
+++ logbook/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from logbook import views
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 
 
 #urlpatterns = [
@@ -13,6 +14,8 @@ from rest_framework.routers import DefaultRouter
 #
 #urlpatterns = format_suffix_patterns(urlpatterns)
                           
+schema_view = get_schema_view(title='Pastebin API')
+
 router = DefaultRouter()
 # /logbook/climbs/
 router.register(r'climbs', views.ClimbViewSet)
@@ -20,6 +23,7 @@ router.register(r'climbs', views.ClimbViewSet)
 router.register(r'users', views.UserViewSet)
 
 urlpatterns = [
+        url('^schema/$', schema_view),
         url(r'^', include(router.urls)),
         url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
     ]
