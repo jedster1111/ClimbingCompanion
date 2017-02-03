827eaacdd5fdb04dbf49de26f181e10d3335bd23
Jed <jedster1111@hotmail.co.uk>
2017-02-03 07:41:21 +0000

    this has been a hug failur but just in case I need to come back to this I'll save xD was trying to create a single page app but bit off more than I could chew, back to something simpler

---
+++ logbook/static/logbook/js/controllers.js
@@ -0,0 +1,5 @@
+﻿angular.module('logbookApp', []).controller('ClimbListController', function ($scope, $state, $window, Climb) {
+    $scope.climbs = Climb.query();
+}).controller('ClimbViewController', function ($scope, $stateParams, Climb) {
+    $scope.Climb = Climb.get({ id: $stateParams.id });
+});
---
+++ logbook/static/logbook/js/services.js
@@ -0,0 +1,7 @@
+﻿angular.module('logbookApp.services', []).factory('Climb', function ($resource) {
+    return $resource('http://127.0.0.1:8000/logbook/climbs/:id', {
+        update: {
+            method: 'PUT'
+        }
+    });
+});
